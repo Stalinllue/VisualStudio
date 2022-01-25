61e3dc82b315afbbe13eac397ff24a8574fe840a
STALIN WLADIMIR DOMINGUEZ BARRERA <sw.dominguez@itq.edu.ec>
2022-01-24 19:40:11 -0500

    subiendo los archivos

---
+++ PruebaVSII/Payment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaVSII
+{
+    public class Payment
+    {
+        public int  paymentID { get; set; }
+        public int costomerID { get; set; }
+        public int staffID { get; set; }
+        public int rentalID { get; set; }
+        public Boolean amount { get; set; }
+      
+
+    }
+}
