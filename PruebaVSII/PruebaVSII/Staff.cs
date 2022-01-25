8010c6215dc3d500b8f24b1a0b605b910ddddc2e
Stalin llue <61747530+Stalinllue@users.noreply.github.com>
2022-01-24 18:23:30 -0600

    Prueba

---
+++ PruebaVSII/PruebaVSII/Staff.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaVSII
+{
+   public class Staff
+    {
+        public int staffID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int addressID { get; set; }
+        public string email { get; set; }
+        public int storeID { get; set; }
+        public int active { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+
+        public string picture { get; set; }
+        public ICollection<Staff> Lista { get; set; }
+    }
+}
