aae85ec464880d2b89f4e2899fa247896e47065b
Stalin llue <61747530+Stalinllue@users.noreply.github.com>
2022-02-07 16:51:14 -0600

    Proyecto Car
    Stalin Dominguez Marco Taco

---
+++ Carproyecto/Aspirante.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carproyecto
+{
+    public class Aspirante
+    {
+        // Atributos
+        public int AspiranteID { get; set; }
+        public int cedulaAsp { get; set; }
+        public string NombreASP { get; set; }
+        public string ApellidoAsp { get; set; }
+        public string correoAsp { get; set; }
+        // Relación con matrículas
+        public List<Matricula> Matriculas { get; set; }
+
+
+
+    }
+}
